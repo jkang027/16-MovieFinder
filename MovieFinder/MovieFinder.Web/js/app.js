b9dc726bbc650a7668dc54644c0b5570ad5d0ed5
Jun Kang <jkang027@yahoo.com>
2016-02-03 17:00:17 -0800

    base assignment done

---
+++ MovieFinder/MovieFinder.Web/js/app.js
@@ -0,0 +1,16 @@
+﻿angular.module('app', []);
+
+angular.module('app').controller('indexController', function ($scope, $http) {
+
+    $scope.searchTerm = "";
+
+    $scope.searchForMovie = function () {
+        $http.get('http://www.omdbapi.com/?s=' + $scope.searchTerm)
+                .success(function (data) {
+                    $scope.movies = data.Search;
+                })
+                .error(function (error) {
+                    alert(error);
+                });
+    };
+});
